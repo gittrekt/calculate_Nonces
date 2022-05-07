6ca8ad3078926ef0622149ebcca3b31a6df1957c
gittrekt <76663968+gittrekt@users.noreply.github.com>
2022-05-07 12:46:57 -0400

    Update calculate_nonces.py

--- calculate_nonces.py
+++ calculate_nonces.py
@@ -30,19 +30,35 @@ def get_diskinfo(loc):
         text = 'TB'
     elif size >= GB:
         calc = GB
+        
+def getDiskSize(dev):
+    return int(open(f"/sys/block/{os.path.basename(dev)}/size").read()) * SECTOR_SIZE
+
+def get_diskinfo(loc):
+    df_output_lines = [s.split() for s in os.popen(f"df {path} -P").read().splitlines()]
+    disk = df_output_lines[1][0]
+    mount = df_output_lines[1][5]
+    size = getDiskSize(df_output_lines[1][0]) - 2 * SECTOR_SIZE
+    calc = MB
+    text = 'MB'
+    if size >= TB:
+        calc = TB
+        text = 'TB'
+    elif size >= GB:
+        calc = GB
         text = 'GB'
     statvfs = os.statvfs(df_output_lines[1][5])
     total_blocks = statvfs.f_frsize * statvfs.f_blocks
     free_blocks = statvfs.f_frsize * statvfs.f_bfree
     available_blocks = statvfs.f_frsize * statvfs.f_bavail
     print(f"""
-Disk           : {disk} 
+Disk           : {disk}
 Mount          : {mount}
 Disk size      : {size / calc:,.2f}{text}
 Avaliable size : {available_blocks / calc:,.2f}{text}
 Total nonces   : {size // NONCE_SIZE:,}
 Free nonces    : {available_blocks // NONCE_SIZE:,}\n""")
-
+    
 if __name__ == "__main__":
     print(f"{sys.argv[0].split('.')[0]} V{VERSION}")
     if len(sys.argv) < 2:
